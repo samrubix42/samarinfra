429714842c51b037860c14b6fbe61a01f7f5bc37
Saumy Anand <samcool3203@gmail.com>
2026-03-02 14:20:44 +0530

    livewire aseet added

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/','pages::home.home')->name('home');
 Route::livewire('/about','pages::home.home')->name('about');
 Route::livewire('/contact','pages::home.home')->name('contact');
 Route::livewire('/service','pages::home.home')->name('service');
+
+Route::get('clear-cache',function(){
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return redirect()->back();
+})->name('clear-cache');
